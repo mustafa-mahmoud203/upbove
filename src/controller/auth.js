e09007ee51b31a7ebabdcaf61b16648a07aab85c
mustafa-mahmoud203 <must.afa000262@gmail.com>
2023-11-12 14:38:19 +0200

    create signUp end point

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -3,11 +3,14 @@ import { asyncHandler } from "../utils/errorHandling.js";
 import { hahs } from "../utils/hashAndCompare.js";
 
 export const signUp = asyncHandler(async (req, res, next) => {
-  const { gender, age, password, email, lastName, firstName } = req.body;
+  const data = req.body;
 
-  const cheackUser = await userModel.findOne({ email });
+  const cheackUser = await userModel.findOne({ email: data.email });
   if (cheackUser) return next(new Error("email exist", { cause: 400 }));
 
-  const hahsPassword = hahs(password);
-  console.log(hahsPassword);
+  const hahsPassword = hahs(data.password);
+  data.password = hahsPassword;
+
+  const user = await userModel.create(data);
+  return res.status(201).json({ message: "Done", user });
 });
